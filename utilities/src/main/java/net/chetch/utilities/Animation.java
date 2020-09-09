f9f835d2b16617a498fd1d3fe4a8693c571c957e
billchetch <bill@chetch.net>
2020-09-09 16:39:21 +0800

    again

--- utilities/src/main/java/net/chetch/utilities/Animation.java
+++ utilities/src/main/java/net/chetch/utilities/Animation.java
@@ -29,6 +29,7 @@ public class Animation {
         return colorAnimation;
     }
 
+    //flashes a drawable ... use repeat = Animation.INIFITE for infinite repeat
     static public ValueAnimator flash(final Drawable drawable, int fromColour, int toColour, int duration, int repeat){
         ValueAnimator colorAnimation = ValueAnimator.ofArgb(fromColour, toColour, fromColour);
         colorAnimation.setDuration(duration);
